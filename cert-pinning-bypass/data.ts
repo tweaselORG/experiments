ce7ef9ab0a4d9d97a1b7de79e9a901c5d4800739
Benjamin Altpeter <hi@bn.al>
2023-06-22 16:27:03 +0200

    Cert pinning: Look at domains

--- cert-pinning-bypass/data.ts
+++ cert-pinning-bypass/data.ts
@@ -47,6 +47,61 @@ WHERE appId NOT IN (
         )
         .all();
     console.log(`Apps per method with any TLS error:`, appsPerMethodWithAnyTlsError);
+
+    // https://github.com/tweaselORG/meta/issues/16#issuecomment-1602683111 :(
+    const domainFromErrorMessage = (error: string) => {
+        for (const regex of [
+            /The client disconnected during the handshake. If this happens consistently for (.+?),/,
+            /The client does not trust the proxy's certificate for (.+?) /,
+        ]) {
+            const match = error.match(regex);
+            if (match) return match[1];
+        }
+    };
+    const runs = (
+        db.prepare('SELECT appId, method, tlsErrors FROM runs;').all() as {
+            appId: string;
+            method: string;
+            tlsErrors: string;
+        }[]
+    )
+        .map((r) => ({
+            ...r,
+            tlsErrors: JSON.parse(r.tlsErrors).map((e) => {
+                const serverDomain = domainFromErrorMessage(e.context.error);
+                return { ...e, serverDomain };
+            }),
+        }))
+        .map((r) => ({
+            ...r,
+            domainsWithTlsError: new Set(r.tlsErrors.map((e) => e.serverDomain).filter((d) => !!d)),
+        }));
+
+    const domainsWithTlsErrorWithoutBypass = new Set(
+        runs
+            .filter((r) => r.method !== 'none')
+            .flatMap((r) => r.tlsErrors)
+            .map((e) => e.serverDomain)
+            .filter((d) => !!d)
+    );
+    console.log('Domains with TLS error without bypass:', domainsWithTlsErrorWithoutBypass);
+
+    const solvedDomains = {
+        objection: new Set<string>(),
+        httptoolkit: new Set<string>(),
+    };
+    for (const run of runs) {
+        if (run.method === 'none' || run.tlsErrors.length === 0) continue;
+
+        const runForNone = runs.find((r) => r.appId === run.appId && r.method === 'none');
+        if (!runForNone) throw new Error(`No run for none found for ${run.appId}. That should never happen.`);
+
+        for (const domain of run.domainsWithTlsError) {
+            if (runForNone.domainsWithTlsError.has(domain)) solvedDomains[run.method].add(domain);
+        }
+    }
+
+    console.log('Solved domains per method:', solvedDomains);
 })();
 
 process.on('exit', () => db.close());
