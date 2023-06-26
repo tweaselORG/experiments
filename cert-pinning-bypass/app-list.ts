21429c5cb6c696d7c989516ca31f042ba88d37dd
Benjamin Altpeter <hi@bn.al>
2023-06-26 14:17:46 +0200

    Cert pinning: Also download games

--- cert-pinning-bypass/app-list.ts
+++ cert-pinning-bypass/app-list.ts
@@ -4,7 +4,7 @@ import { fetchTopCharts, categories } from 'parse-play';
     const entries = (
         await fetchTopCharts(
             Object.keys(categories)
-                .filter((c) => !c.startsWith('GAME'))
+                .filter((c) => !c.startsWith('GAME_'))
                 .map((id) => ({
                     category: id as 'APPLICATION',
                     chart: 'topselling_free',
